67209bffdbd618ef99c65769a0e64013c9038593
HarishKamaras <harishkamarasu@gmail.com>
2022-05-10 16:10:18 +0530

    GamblingSimulationProblem

---
+++ src/com/bridgelabz/gamblingsimulationproblem/GambleMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.gamblingsimulationproblem;
+
+public class GambleMain {
+    public static void main(String[] args) {
+        System.out.println(" Welcome to Gamble game");
+    }
+}
