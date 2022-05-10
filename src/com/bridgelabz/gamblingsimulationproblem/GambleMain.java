0830625bc5be283c47a987c6bdc564219ec716b3
HarishKamaras <harishkamarasu@gmail.com>
2022-05-10 17:56:34 +0530

    LuckiestAndUnLuckiestDay

--- src/com/bridgelabz/gamblingsimulationproblem/GambleMain.java
+++ src/com/bridgelabz/gamblingsimulationproblem/GambleMain.java
@@ -1,11 +1,11 @@
 package com.bridgelabz.gamblingsimulationproblem;
-
 public class GambleMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Gamble game ");
-        //UC5 total money for month
+        //UC6 find luckiest and unLuckiest day
+        int totalInvestedMoney = 3000;
         GamblerGame objectGamble = new GamblerGame();
-        objectGamble.gamble(); //Month1
-        objectGamble.gamble(); //month2
+        int totalMoneyForMonth = objectGamble.gamble();
+        System.out.println("the total money on first month: "+ totalMoneyForMonth);
     }
 }
--- src/com/bridgelabz/gamblingsimulationproblem/GamblerGame.java
+++ src/com/bridgelabz/gamblingsimulationproblem/GamblerGame.java
@@ -5,21 +5,17 @@ public class GamblerGame {
         int betEverygame = 1;
         int totalMoney = 0;
         int currentDayMoney = 100;
-
-        //UC5 Each Month Days lose and won
+        int maxwinday = 0;
+        int maxloseday = 0;
+        int luckiestday = 0;
+        int unluckiestday = 0;
         for (int i = 0; i < 30; i++) {
             int j = 0;
             int win = 0;
             int lose=0;
-
             int startingMoney =100;
-
             while ((startingMoney * 100) / 100 > 50 && (startingMoney * 100) / 100 < 150) {
-                // win = 0;
-                //lose =0;
                 int bet = (int) (Math.random() * 10) % 2;
-                // j=1;
-                //     System.out.println("how many timePlayed : " + j);
                 if (bet == 1) {
                     startingMoney = startingMoney + 1;
                     //           System.out.println("win currentmoney : " + startingMoney);
@@ -29,22 +25,34 @@ public class GamblerGame {
                     //             System.out.println("lost currentmoney : " + startingMoney);
                     lose++;
                 }
-                //currentMoneyday1 = startingMoney;
                 j++;
-
             }
-            System.out.println("*******************day:" + i+"********************");
+            if(maxwinday < win )
+            {
+                maxwinday = win;
+                luckiestday = i;
+            }
+            if(maxloseday < lose){
+                maxloseday =lose;
+                unluckiestday =i;
+            }
+            System.out.println("********************day:"+i+"******************");
             System.out.println("how many times played : "+j);
-            System.out.println( "how many times win : "+win);
+            System.out.println( " how many times win : "+win);
             System.out.println("how many times lose : "+lose);
-            //if()
+            System.out.println("luckiest day : "+luckiestday+ " Money: "+maxwinday);
+            System.out.println("Unluckiest day : "+unluckiestday+ "  Money: "+maxloseday);
             currentDayMoney = startingMoney;
             totalMoney = totalMoney + currentDayMoney;
-            //System.out.println("**************************************day:"+i);
-            System.out.println("total money per day : " + currentDayMoney);
+            System.out.println("total money per day" + currentDayMoney);
             System.out.println("**************************************");
         }
-        System.out.println("total money after month : "+totalMoney);
+        System.out.println();
+        System.out.println("#############MonthlyResult#################");
+        System.out.println("total money after this month : "+totalMoney);
+        System.out.println("luckiest day : "+luckiestday+ " Money: "+maxwinday);
+        System.out.println("Unluckiest day : "+unluckiestday+ "  Money: "+maxloseday);
+        System.out.println("############################################");
         return totalMoney;
     }
 }
