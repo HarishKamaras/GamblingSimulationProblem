8aba0239d58774523ac532b877820c211537db55
HarishKamaras <harishkamarasu@gmail.com>
2022-05-10 16:15:42 +0530

    StartingMoney

--- src/com/bridgelabz/gamblingsimulationproblem/GambleMain.java
+++ src/com/bridgelabz/gamblingsimulationproblem/GambleMain.java
@@ -2,6 +2,10 @@ package com.bridgelabz.gamblingsimulationproblem;
 
 public class GambleMain {
     public static void main(String[] args) {
+        int startingMoney = 100;
+        int betEverygame = 1;
         System.out.println(" Welcome to Gamble game");
+        System.out.println("starting money : " +startingMoney);
+        System.out.println("betEverygame : " +betEverygame);
     }
 }
