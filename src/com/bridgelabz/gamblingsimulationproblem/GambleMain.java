8a3fdc2e4e7b6ea392c1f4db69ef712a110baebd
HarishKamaras <harishkamarasu@gmail.com>
2022-05-10 18:40:29 +0530

    IfWonPlayNextMonth

--- src/com/bridgelabz/gamblingsimulationproblem/GambleMain.java
+++ src/com/bridgelabz/gamblingsimulationproblem/GambleMain.java
@@ -2,7 +2,7 @@ package com.bridgelabz.gamblingsimulationproblem;
 public class GambleMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Gamble game ");
-        //UC7 If have won money than he invested then play for the month
+        //UC7 if have won money than he invested then play for the month
         int totalInvestedMoney = 3000;
         GamblerGame objectGamble = new GamblerGame();
         int totalMoneyForMonth = objectGamble.gamble();
