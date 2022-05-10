c5f7de42ce58bef7dceda221a657131445ab1a44
HarishKamaras <harishkamarasu@gmail.com>
2022-05-10 18:37:41 +0530

    if(totalMoneyForMonth>totalInvestedMoney){

--- src/com/bridgelabz/gamblingsimulationproblem/GambleMain.java
+++ src/com/bridgelabz/gamblingsimulationproblem/GambleMain.java
@@ -2,10 +2,17 @@ package com.bridgelabz.gamblingsimulationproblem;
 public class GambleMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Gamble game ");
-        //UC6 find luckiest and unLuckiest day
+        //UC7 If have won money than he invested then play for the month
         int totalInvestedMoney = 3000;
         GamblerGame objectGamble = new GamblerGame();
         int totalMoneyForMonth = objectGamble.gamble();
         System.out.println("the total money on first month: "+ totalMoneyForMonth);
+        if(totalMoneyForMonth>totalInvestedMoney){
+            totalMoneyForMonth= objectGamble.gamble();
+            System.out.println("the total money on next month: "+ totalMoneyForMonth);
+        }
+        else {
+            System.out.println("*********************Stop Gambling*****************************");
+        }
     }
 }
