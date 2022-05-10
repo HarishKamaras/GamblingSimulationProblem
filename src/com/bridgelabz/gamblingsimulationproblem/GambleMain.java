181653f975ea8715224c8927b9234a03d576e103
HarishKamaras <harishkamarasu@gmail.com>
2022-05-10 17:26:30 +0530

    TotalmoneyAfter20Days

--- src/com/bridgelabz/gamblingsimulationproblem/GambleMain.java
+++ src/com/bridgelabz/gamblingsimulationproblem/GambleMain.java
@@ -2,34 +2,36 @@ package com.bridgelabz.gamblingsimulationproblem;
 
 public class GambleMain {
     public static void main(String[] args) {
-        int startingMoney = 100;
         int betEverygame = 1;
-        int win = 0;
-        int lose = 0;
         int currentDayMoney = 100;
+        int totalMoney = 0;
         System.out.println(" Welcome to Gamble game");
-        System.out.println("starting money : " + startingMoney);
         System.out.println("betEverygame : " + betEverygame);
-        //UC3 if win or lose 50% of stake resign the game
-        int j = 1;
-        while ((startingMoney * 100) / 100 > 50 && (startingMoney * 100) / 100 < 150) {
-
-            int bet = (int) (Math.random() * 10) % 2;
-
-                System.out.println("how many timePlayed : " + j);
-            if (bet == 1) {
-                startingMoney = startingMoney + 1;
-                System.out.println("win currentmoney : " + startingMoney);
-                win++;
-            } else {
-                startingMoney = startingMoney - 1;
-                System.out.println("lost currentmoney : " + startingMoney);
-                lose++;
+        //UC4 Total money after 20 days of gamble
+        for (int i = 0; i < 20; i++) {
+            int j = 0;
+            int win = 0;
+            int lose = 0;
+            int startingMoney = 100;
+            while ((startingMoney * 100) / 100 > 50 && (startingMoney * 100) / 100 < 150) {
+                int bet = (int) (Math.random() * 10) % 2;
+                if (bet == 1) {
+                    startingMoney = startingMoney + 1;
+                    win++;
+                } else {
+                    startingMoney = startingMoney - 1;
+                    lose++;
+                }
+                j++;
             }
+            System.out.println("*******************day:" + i+"********************");
+            System.out.println("how many times played : " + j);
+            System.out.println("how many times win : " + win);
+            System.out.println("how many times lose : " + lose);
             currentDayMoney = startingMoney;
-            j++;
+            totalMoney = totalMoney + currentDayMoney;
+            System.out.println("total money per day" + currentDayMoney);
+            System.out.println("**************************************");
         }
-        System.out.println("*****************************************");
-        System.out.println("Total money on per Day : "+currentDayMoney);
-    }
-}
+        System.out.println("total money after 20days : "+totalMoney);
+    }}
